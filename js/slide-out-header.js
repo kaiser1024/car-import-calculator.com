31ae1ce69fb412a6db769e0dc60f6d9f6e73415d
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-03 15:33:30 +0300

    Update slide-out-header.js

--- js/slide-out-header.js
+++ js/slide-out-header.js
@@ -4,12 +4,12 @@ let lastScroll = window.pageYOffset;
 window.addEventListener('scroll', () => {
   const currentScroll = window.pageYOffset;
 
-  if (currentScroll <= 0 {
+  if (currentScroll <= 0) {
     topbar.classList.remove('hidden');
     return;
   }
 
-if (Math.abs(currentScroll - lastScroll < 5) return;
+if (Math.abs(currentScroll - lastScroll) < 5 return;
 
 if (currentScroll > lastScroll) {
     topbar.classList.add('hidden');
