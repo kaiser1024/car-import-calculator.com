49ad850373cca3674c95a127d81bf6a93027a826
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-03 13:46:09 +0300

    Update dummy.js

--- js/dummy.js
+++ js/dummy.js
@@ -2,7 +2,7 @@
 fetch('html/dummy.html')
   .then(res => res.text())
   .then(html => {
-    const container = document.querySelector('#faq .container');
+    const container = document.querySelector('.faq .container');
     if (container) {
       container.innerHTML = html;
     }
