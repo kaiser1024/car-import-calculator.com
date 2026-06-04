5d262301758e0b4ab616c25075696dc06f69c123
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-04 10:32:54 +0300

    Update load-calculator.js

--- js/load-calculator.js
+++ js/load-calculator.js
@@ -1,1 +1,8 @@
-
+fetch('./html/calculator.html')
+  .then(res => res.text())
+  .then(html => {
+    const container = document.querySelector('.calculator');
+    if (container) {
+      container.innerHTML = html;
+    }
+  });
