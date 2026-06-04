2c56f1e572712004dd3dd737d08a889dbdc97da0
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-04 13:29:13 +0300

    Delete js/load-calculator.js

--- js/load-calculator.js
+++
@@ -1,8 +0,0 @@
-fetch('./html/calculator.html')
-  .then(res => res.text())
-  .then(html => {
-    const calculator = document.querySelector('.calculator');
-    if (calculator) {
-      calculator.innerHTML = html;
-    }
-  });
