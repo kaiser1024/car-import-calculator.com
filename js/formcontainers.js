2b3f3e3dd5af4032a04dfbe63fdfc8bf6b977f01
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-09 13:59:06 +0300

    Update formcontainers.js

--- js/formcontainers.js
+++ js/formcontainers.js
@@ -2,5 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const formcontainers = document.querySelectorAll('[class*="loadform-"]');
 
+    console.log(formcontainers);
+
+    const getformids = document.createElement("script");
+    getformids.src = "./js/getformids.js";
+
+    getformids.onload = function () {
+        getFormIDs(formcontainers);
+    };
+
+    document.body.appendChild(getformids);
+
 });
-``
