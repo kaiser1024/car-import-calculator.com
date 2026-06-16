c31f8dfa3bf04e72b2e27111a5ce66609700e74f
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 15:46:27 +0300

    Update html.js

--- home/js/html.js
+++ home/js/html.js
@@ -1,3 +1,8 @@
 const langScript = document.createElement('script');
+const pagetitleScript = document.createElement('script');
+
 langScript.src = './js/html/1-lang.js';
+pagetitleScript.src = './js/html/1-pagetitle.js';
+
 document.head.appendChild(langScript);
+document.head.appendChild(pagetitleScript);
