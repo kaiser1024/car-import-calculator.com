8edcdaf621ce1d0d8939124c31c068e9d0dfe73a
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 19:38:21 +0300

    Remove script loading for CSS, DB, and HTML
    Removed script loading for CSS, DB, and HTML manager.

--- home/0.js
+++ home/0.js
@@ -1,14 +1,2 @@
-// load CSS
-const cssScript = document.createElement('script');
-cssScript.src = 'css.js';
-document.head.appendChild(cssScript);
 
-// load DB
-const dbScript = document.createElement('script');
-dbScript.src = './js/db.js';
-document.head.appendChild(dbScript);
 
-// load HTML manager
-const htmlScript = document.createElement('script');
-htmlScript.src = './js/html.js';
-document.head.appendChild(htmlScript);
