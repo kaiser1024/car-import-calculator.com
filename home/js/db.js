d3bc479e451371dad390e532fdacc4819afeca5c
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 15:57:31 +0300

    Update db.js

--- home/js/db.js
+++ home/js/db.js
@@ -1,3 +1,27 @@
 window.DB = {
-  language: 'en'
+  language: 'en',
+
+  title: {
+    en: 'This is the Page Title',
+    de: 'Dies ist der Seitentitel',
+    fr: 'Ceci est le titre de la page',
+    es: 'Este es el título de la página',
+    ca: 'Aquest és el títol de la pàgina',
+    pt: 'Este é o título da página',
+    it: 'Questo è il titolo della pagina',
+    da: 'Dette er sidetitlen',
+    sv: 'Detta är sidans titel',
+    no: 'Dette er sidetittelen',
+    is: 'Þetta er titill síðunnar',
+    pl: 'To jest tytuł strony',
+    sk: 'Toto je názov stránky',
+    cs: 'Toto je název stránky',
+    sl: 'To je naslov strani',
+    hr: 'Ovo je naslov stranice',
+    sr: 'Ovo je naslov stranice',
+    sq: 'Ky është titulli i faqes',
+    hu: 'Ez az oldal címe',
+    ro: 'Acesta este titlul paginii',
+    bg: 'Това е заглавието на страницата'
+  }
 };
