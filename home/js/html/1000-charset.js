1622a2e8cdde39d2ee094c45313e6c3ac5a889f9
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 19:09:47 +0300

    Set charset to UTF-8 in meta tag

--- home/js/html/1000-charset.js
+++ home/js/html/1000-charset.js
@@ -2,6 +2,6 @@ const htmlHead = document.querySelector('head');
 
 if (htmlHead && !htmlHead.querySelector('meta[charset]')) {
     const htmlMeta = document.createElement('meta');
-    htmlMeta.setAttribute('charset', '');
+    htmlMeta.setAttribute('charset', 'UTF-8');
     htmlHead.prepend(htmlMeta);
 }
