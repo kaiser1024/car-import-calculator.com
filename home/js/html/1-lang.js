ab4d9395d41b0314e861e87bc18cc4872a4d199a
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 15:50:22 +0300

    Update 1-lang.js

--- home/js/html/1-lang.js
+++ home/js/html/1-lang.js
@@ -1,1 +1,9 @@
+// set <html lang=""> based on DB, fallback to 'en'
 
+let lang = 'en';
+
+if (window.DB && typeof window.DB.language === 'string' && window.DB.language.trim() !== '') {
+  lang = window.DB.language;
+}
+
+document.documentElement.lang = lang;
