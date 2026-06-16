86e646ac3a1453bd1a1f7679dc3e62290b4d8b68
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 11:52:12 +0300

    Update css.js

--- home/js/css.js
+++ home/js/css.js
@@ -1,12 +1,12 @@
-// load CSS files from /root-css/
+// load CSS files from /css/
 
 const cssFiles = [
-  '0.css' // placeholder – add more files here later
+  '0.css' // placeholder
 ];
 
 cssFiles.forEach(file => {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = `/root-css/${file}`;
+  link.href = `/css/${file}`;
   document.head.appendChild(link);
 });
