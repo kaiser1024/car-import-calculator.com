07c435b3412931179aa4ccc805a6de85acd498b7
kaiser1024 <kaiser.laszlo@gmail.com>
2026-06-16 19:15:56 +0300

    Add charset script to HTML head

--- home/js/html.js
+++ home/js/html.js
@@ -1,8 +1,12 @@
+const charsetScript = document.createElement('script');
 const langScript = document.createElement('script');
 const pagetitleScript = document.createElement('script');
 
+charsetScript.src = './js/html/1-charset.js';
 langScript.src = './js/html/1-lang.js';
 pagetitleScript.src = './js/html/1-pagetitle.js';
 
+document.head.appendChild (charsetScript);
 document.head.appendChild(langScript);
 document.head.appendChild(pagetitleScript);
+
